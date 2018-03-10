bb5405895a1bf2d69892d84ee48852d5e7c496ae
Bastian MOrath <bastian.ethz@gmail.com>
2018-03-10 18:13:34 +0000

    Added Plot with HR/Difficulty Correlation

--- factory.py
+++ factory.py
@@ -5,46 +5,53 @@ import globals as gl
 '''
 
 
-class Helpers:
-    @staticmethod
-    def get_colors():
-        colors = ['red'] * len(gl.conc_dataframes)
-        for idx, user_id in enumerate(gl.conc_dataframes.groupby(['userID', 'round'])['userID'].max()):
-            if user_id % 2 == 0:
-                colors[idx] = '#68829E'
-            else:
-                colors[idx] = '#AEBD38'
-        return colors
+def get_colors():
+    colors = ['red'] * len(gl.conc_dataframes)
+    for idx, user_id in enumerate(gl.conc_dataframes.groupby(['userID', 'round'])['userID'].max()):
+        if user_id % 2 == 0:
+            colors[idx] = '#68829E'
+        else:
+              colors[idx] = '#AEBD38'
+    return colors
 
 
-class Filter:
-    ''' Since the MOVEMENTTUTORIAL at the beginning is different for each player/logfile,
-        we need to align them, i.e. remove tutorial-log entries and then set timer to 0
-    '''
-    @staticmethod
-    def remove_movement_tutorials():
-        for idx, df in enumerate(gl.dataframes):
-            if 'MOVEMENTTUTORIAL' in df['Gamemode'].values:
+''' Since the MOVEMENTTUTORIAL at the beginning is different for each player/logfile,
+    we need to align them, i.e. remove tutorial-log entries and then set timer to 0
+'''
+def remove_movement_tutorials():
+    for idx, df in enumerate(gl.dataframes):
+        if 'MOVEMENTTUTORIAL' in df['Gamemode'].values:
 
-                tutorial_mask = df['Gamemode']=='MOVEMENTTUTORIAL'
-                tutorial_entries = df[tutorial_mask]
-                tutorial_endtime = tutorial_entries['Time'].max()
+            tutorial_mask = df['Gamemode']=='MOVEMENTTUTORIAL'
+            tutorial_entries = df[tutorial_mask]
+            tutorial_endtime = tutorial_entries['Time'].max()
 
-                df['Time'] = df['Time'].apply(lambda x: x - tutorial_endtime)
-                gl.dataframes[idx] = df[~tutorial_mask].reset_index(drop=True)
+            df['Time'] = df['Time'].apply(lambda x: x - tutorial_endtime)
+            gl.dataframes[idx] = df[~tutorial_mask].reset_index(drop=True)
 
-    ''' Since the SHIELDTUTORIAL at the beginning is different for each player/logfile, 
-        we need to remove them and adjust timer
-    '''
-    @staticmethod
-    def remove_shield_tutorial():
-        for idx, df in enumerate(gl.dataframes):
-            if 'SHIELDTUTORIAL' in df['Gamemode'].values: 
-                tutorial_mask = df['Gamemode'] == 'SHIELDTUTORIAL'
-                tutorial_entries = df[tutorial_mask]
-                start_time = tutorial_entries['Time'].min()
-                end_time = tutorial_entries['Time'].max()
-                tutorial_endtime = end_time - start_time
+''' Since the SHIELDTUTORIAL at the beginning is different for each player/logfile, 
+    we need to remove them and adjust timer
+'''
+@staticmethod
+def remove_shield_tutorial():
+     for idx, df in enumerate(gl.dataframes):
+        if 'SHIELDTUTORIAL' in df['Gamemode'].values: 
+            tutorial_mask = df['Gamemode'] == 'SHIELDTUTORIAL'
+            tutorial_entries = df[tutorial_mask]
+            start_time = tutorial_entries['Time'].min()
+            end_time = tutorial_entries['Time'].max()
+            tutorial_endtime = end_time - start_time
 
-                df['Time'] = df['Time'][df['Time']>start_time].apply(lambda x: x - tutorial_endtime)
-                gl.dataframes[idx] = df[~tutorial_mask].reset_index(drop=True)
+            df['Time'] = df['Time'][df['Time']>start_time].apply(lambda x: x - tutorial_endtime)
+            gl.dataframes[idx] = df[~tutorial_mask].reset_index(drop=True)
+
+'''Subsitutes difficulties with numbers to work with them in a better way, from 1 to 3
+
+'''
+def transformToNumbers(df):
+    mapping = {'LOW': 1, 'MEDIUM': 2, 'HIGH': 3, 'undef': -1}
+    df = df.replace({'physDifficulty': mapping, 'psyStress': mapping, 'psyDifficulty': mapping})  
+
+    for col in ['physDifficulty', 'psyStress', 'psyDifficulty']:
+        df[col] = df[col].astype('int64')      
+    return df
--- globals.py
+++ globals.py
@@ -5,6 +5,12 @@ import re
 
 import pandas as pd
 import numpy as np
+import datetime
+
+
+
+green_color = '#AEBD38'
+blue_color = '#68829E'
 
 file_expressions = [r'.{0,}.log',
                     r'.{0,}Flitz.{0,}.log',
@@ -22,9 +28,9 @@ def init():
     global conc_dataframes
     init_rel_files()
     init_dataframes()
+    add_timedelta_column()
     add_user_and_round()
     conc_dataframes = pd.concat(dataframes, ignore_index=True)
-
 
 ''' I differentiate between log data that:
         - is from FBMC
@@ -46,7 +52,7 @@ def init_dataframes():
     logs = [logs_path + "/" +  s for s in rel_files]
     column_names = ['Time','Logtype','Gamemode','Points','Heartrate','physDifficulty','psyStress','psyDifficulty','obstacle']
     dataframes = list(pd.read_csv(log, sep=';', skiprows=5, index_col=False, names=column_names) for log in logs)
-
+    
 
 ''' Add user_id and round (1 or 2) as extra column
 '''
@@ -54,11 +60,19 @@ def init_dataframes():
 
 def add_user_and_round():
         for idx, df in enumerate(dataframes):
-            df.insert(9, 'userID', np.full( (len(df.index),1), int(np.floor(idx/2))))
+            df.insert(10, 'userID', np.full( (len(df.index),1), int(np.floor(idx/2))))
             # m = re.search(r'(\d)_[0,1]',rel_files[idx], re.IGNORECASE)
             match = re.search('(\d)_(\d+)', rel_files[idx])  # add if round 0 or 1
             if match:
-                df.insert(10, 'round', np.full( (len(df.index),1), match.group(1)))
+                df.insert(11, 'round', np.full( (len(df.index),1), match.group(1)))
             else:
-                df.insert(10, 'round', np.full( (len(df.index),1), 0))
+                df.insert(11, 'round', np.full( (len(df.index),1), 0))
             dataframes[idx] = df
+
+'''For a lot of queries, it is useful to have the ['Time'] as a timedeltaIndex object
+'''
+def add_timedelta_column():
+    global dataframes
+    for idx, df in enumerate(dataframes):
+        new = df['Time'].apply(lambda x: datetime.timedelta(seconds=x))
+        dataframes[idx] = dataframes[idx].assign(timedelta=new)
