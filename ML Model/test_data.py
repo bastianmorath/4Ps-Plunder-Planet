8235fe6a4676c7c6ac482a870ca304b809052f2b
bastianmorath <bastian.morath@gmx.ch>
2018-04-07 10:44:05 +0200

    First version of synthesized data; HR still random

--- ML Model/test_data.py
+++ ML Model/test_data.py
@@ -1,10 +1,13 @@
-from datetime import timedelta
 import pandas as pd
-
+import numpy as np
+from operator import add
+from scipy.stats import truncnorm
+import itertools
 import globals as gl
 import features_factory as f_factory
 
 num_dataframes = 10 # How many dataframes should be created?
+length_dataframe = 100  # How many rows should one dataframe have?
 mean_hr = 123.9  # Mean of normal distribution of heartrate
 std_hr = 16.8  # std of normal distribution of heartrate
 
@@ -15,13 +18,23 @@ std_hr = 16.8  # std of normal distribution of heartrate
 def init_with_testdata():
 
     for i in range(0, num_dataframes):
-        times = [1,2,3,4,5,6]
-        logtypes = ['EVENT_OBSTACLE', 'EVENT_CRASH',  'EVENT_CRASH', 'EVENT_OBSTACLE', 'EVENT_CRASH', 'EVENT_OBSTACLE']
-        heartrates = [378, 155, 77, 973, 973, 973]
-        timedeltas = [timedelta(seconds =1), timedelta(seconds =2), timedelta(seconds=3), timedelta(seconds=4),
-                      timedelta(seconds =5), timedelta(seconds =6)]
+        X = get_truncated_normal(mean=0, sd=0.2, low=0, upp=0.3)
+        noise = X.rvs(length_dataframe)
+        times = list(map(add,
+                         range(0, length_dataframe),
+                         noise))
+        # EVENT_CRASH has to be followed by an EVENT_OBSTACLE
+        # TODO: those two need to happen withing  milliseconds or so...
+        types = [['EVENT_OBSTACLE', 'EVENT_OBSTACLE'], ['EVENT_CRASH', 'EVENT_OBSTACLE']]
+        random_choices = np.random.choice([0, 1], int(length_dataframe/2), [0.9, 0.1])
+        logtypes = list(itertools.chain.from_iterable([types[i] for i in random_choices]))
+
+        heartrates = np.random.normal(mean_hr, std_hr, length_dataframe)
+        timedeltas = pd.to_timedelta(times, unit='S')
+
         dataframe = pd.DataFrame(data={'Time': times, 'Logtype': logtypes,
-                                         'Heartrate': heartrates, 'timedelta': timedeltas})
+                                        'Heartrate': heartrates, 'timedelta': timedeltas})
+        print(dataframe)
         gl.df_list.append(dataframe)
 
     gl.df_without_features = pd.concat(gl.df_list, ignore_index=True)
@@ -32,3 +45,11 @@ def init_with_testdata():
         }
     )
     print(gl.obstacle_df)
+
+
+'''Returns a value from a normal distribution, truncated to a boundary'''
+
+
+def get_truncated_normal(mean=0, sd=1, low=0, upp=10):
+    return truncnorm(
+        (low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
